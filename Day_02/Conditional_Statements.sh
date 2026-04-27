60145ae47ed5dcb80d37b0cc958a5e74d52e7901
Rajesh Samal <rajeshsamal745@gmail.com>
2026-04-27 14:02:37 +0000

    Added Conditional Statements script

--- Day_02/Conditional_Statements.sh
+++ Day_02/Conditional_Statements.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+#Assign arguments to variable names
+
+fileName=$1
+number=$2
+service=$3
+
 #Check if a file exists
 
-if [ -f "$1" ]; then
-	echo "File exists"
+if [ -f "$fileName" ]; then
+	echo "$fileName exists"
 else
-	echo "File not found"
+	echo "$fileName not found"
+fi
+
+#Check user input
+if [ "$number" -eq 10 ]; then
+	echo "$number is exactly 10"
+elif [ "$number" -gt 10 ]; then
+	echo "$number is greater than 10"
+else
+	echo "$number is smaller than 10"
+fi
+
+#Check a system service
+if systemctl is-active --quiet "$service"; then
+	echo "$service is running"
+else
+	echo "$service is not running"
 fi
